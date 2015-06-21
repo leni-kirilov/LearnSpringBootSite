22b0b992e82e67d652098eaec964b3a75cde48ec
Leni Kirilov <leni.kirilov@gmail.com>
2015-06-21 20:52:50 +0300

    Adding a MockMVC Integration test - improved assembled jar name

---
+++ src/test/java/bg/kirilov/springboot/HelloControllerIT.java
@@ -0,0 +1,45 @@
+package bg.kirilov.springboot;
+
+/**
+ * Created by Leni on 21.6.2015 ?..
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URL;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest({"server.port=0"}) //port is assigned dynamiccally
+public class HelloControllerIT {
+
+    @Value("${local.server.port}") //get the dynamic port
+    private int port;
+
+    private URL base;
+    private RestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/");
+        template = new TestRestTemplate();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        assertThat(response.getBody(), equalTo("Greetings from Spring Boot Leni!"));
+    }
+}
